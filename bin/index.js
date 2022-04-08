d1d6be4b41f53d9ddd640914584dfe030cd25866
Mariana Muniz <magsmuniz@outlook.com>
2022-04-08 07:31:20 -0300

    index

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,9 @@ const bp = require ('body-parser');
 
 const app = express ();
 // verificar essas funções
-const res = require("express/lib/response"); 
-const { app } = require("express/lib/response"); 
-const { ModuleResolutionKind } = require("typescript"); 
+//const res = require("express/lib/response"); 
+//const { app } = require("express/lib/response"); 
+//const { ModuleResolutionKind } = require("typescript"); 
 
 //configurando o parser
 app.user (bp.json({limit: '10mb'}));
@@ -16,15 +16,8 @@ app.set ('viwe engine', 'ejs');
 app.set ('views', 'views');
 
 //definindo arquivos estaticos
-app.use (express.static ('public'));
+app.use (express.static ('public') );
 const professor_route = require ('../src/routes/professor ');
-
-//chamando rotas default
-app.user ('/', (req, res) =>{
-    return res.render ('login');
-    
-})
-
 
 //chamando rotas
 app.use ('/' ,(req, res) =>{
