d40e7e582b5bdb3936158d0a5293f3344d21ba5c
Mariana Muniz <magsmuniz@outlook.com>
2022-04-12 20:56:41 -0300

    alterações

--- src/controller/middleware/auth.js
+++ src/controller/middleware/auth.js
@@ -3,7 +3,10 @@ const jwt = require ('jsonwebtoken');
 
 exports.autorizar = async (req, res, next) =>{
     try{
-
+        const resultado = await storage.getInLocal('login')
+        if (!resultado) {
+            return res.send ('Permissão negada');
+        }
     }catch (error){
         next (err);
     }
--- src/controller/middleware/models/resources/routes/funcionario.js
+++ src/controller/middleware/models/resources/routes/funcionario.js
@@ -2,9 +2,11 @@ const funcionario = require ('../controller/funcionario');
 const express = require ('express');
 const { route } = require('express/lib/application');
 const router = express.Router ( );
+const auth = require ('../middleware/auth');
+
 
 router.get(' / ', Funcionario.getCriar);
-router.get(' / todos', Funcionario.buscarTodos);
+router.get(' / todos', auth.autorizar,  Funcionario.buscarTodos);
 router.get('/ logar', Funcionario.getLogar);
 router.post(' / ', Funcionario.postCriar);
 router.post ('/logar', Funcionario.postLogar);
