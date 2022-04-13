524710710af67c7b8c2d64eee393bd621c487c71
Mariana Muniz <magsmuniz@outlook.com>
2022-04-13 11:29:52 -0300

    começando front

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,35 @@
-const express = require ('express');
-const bp = require ('body-parser');
+const express = require('express');
+const bp = require('body-parser');
 
-const app = express ();
+const app = express();
 // verificar essas funções
 //const res = require("express/lib/response"); 
 //const { app } = require("express/lib/response"); 
 //const { ModuleResolutionKind } = require("typescript"); 
 
 //configurando o parser
-app.user (bp.json({limit: '10mb'}));
-app.user (bp.urlencoded({extendend: false}));
+app.user(bp.json({ limit: '10mb' }));
+app.user(bp.urlencoded({ extendend: false }));
 
 //configurando o front
-app.set ('viwe engine', 'ejs');
-app.set ('views', 'views');
+app.set('views engine', 'ejs');
+app.set('views', 'views');
 
 //definindo arquivos estaticos
-app.use (express.static ('public') );
+app.use(express.static('public'));
 
-const professor_route = require ('../src/routes/professor ');
-const funcionario_route=require ('../src/routes/funcionario');
+const professor_route = require('../src/routes/professor ');
+const funcionario_route = require('../src/routes/funcionario');
 
-app.use (' /  professor', professor_route);
-app.use ('/ funcionario', funcionario_route);
+app.use(' /  professor', professor_route);
+app.use('/ funcionario', funcionario_route);
 
 
 //chamando rotas default
-app.use ('/' ,(req, res) =>{
-        return res.send (' Bem Vindo');
+app.use('/', (req, res) => {
+    return res.send(' Bem Vindo');
 
 });
 
 //exportando aplicação
 Module.exports = app;
-
-
--- src/controller/funcionario.js
+++ src/controller/funcionario.js
@@ -1,81 +1,79 @@
 const res = require('express/lib/response');
 const { servicesVersion } = require('typescript');
-const funcionario = require ('../resources/funcionario');
-const cript = require ('bcrypt');
-const auth = require ('../middleware/auth');
-const storage = require ('localtoken');
+const funcionario = require('../resources/funcionario');
+const cript = require('bcrypt');
+const auth = require('../middleware/auth');
+const storage = require('localtoken');
 
 
 //fazer registro do funcionario
-exports.getCriar - async (req, res, next) => {
+exports.getCriar - async(req, res, next) => {
     try {
-        return res.send ('pagina funcionando');
-        } catch (err){
-            next(err);
-        }
-}
-exports.getLogar = async (req, res,next) =>{
-    try {
-        return res.send ('pagina de login');
-        } catch (err){
-            next(err);
-
-}
-
-exports.postLogar = async (req, res, next) =>{
-    try {
-            const resultado = await Funcionario.validarEntrada(req.body);
-            if(!resultado){
-                return res.send ('Usúario não encontrado');
-            }
-            if (!await cript.compare(req.body.senha, resultado.senha)){
-                return res.send ('Senha errada');
-
-            }
-            const token = await auth.gerarToken ( { resultado});
-            storage.setInLocal('login',token);
-            console.log ('usuario logado com sucesso');
-            return res.redirect ('/');
-                    } catch (err){
-            next(err);
-    
-}
-
-exports.deslogar =  (req, res)=>{
-    try{
-      await  storage.removeLocal('login');
-      return res.json (
-          {
-              Success: 'Deslogado com sucesso'
-          }
-      )
-    }catch (err){
-        next (err);
-    }
-
-}
-
-exports.postCriar = async (req, res, next) =>{
-    try{
-           let resultado = await funcionario.validarRegistro( );
-           if (!resultado){
-               let funcionario = await Funcionario.criar (req, body);
-               return res.json (funcionario);
-           } else {
-               return res.json ( {
-                   error: 'funcionario já cadastrado'
-               })
-           }
-       }  catch (err) {
+        return res.send('pagina funcionando');
+    } catch (err) {
         next(err);
-
     }
 }
-exports.buscarTodos = async (req, res, next) =>{
-    try{
-        const todos = await Funcionario.buscarTodos( );
-        return res.json (todos);
-     }catch (err){
-         next (err);
-     }
-}
+exports.getLogar = async(req, res, next) => {
+        try {
+            return res.render('login/_index');
+        } catch (err) {
+            next(err);
+
+        }
+
+        exports.postLogar = async(req, res, next) => {
+                try {
+                    const resultado = await Funcionario.validarEntrada(req.body);
+                    if (!resultado) {
+                        return res.send('Usúario não encontrado');
+                    }
+                    if (!await cript.compare(req.body.senha, resultado.senha)) {
+                        return res.send('Senha errada');
+
+                    }
+                    const token = await auth.gerarToken({ resultado });
+                    storage.setInLocal('login', token);
+                    console.log('usuario logado com sucesso');
+                    return res.redirect('/');
+                } catch (err) {
+                    next(err);
+
+                }
+
+                exports.deslogar = (req, res) => {
+                    try {
+                        await storage.removeLocal('login');
+                        return res.json({
+                            Success: 'Deslogado com sucesso'
+                        })
+                    } catch (err) {
+                        next(err);
+                    }
+
+                }
+
+                exports.postCriar = async(req, res, next) => {
+                    try {
+                        let resultado = await funcionario.validarRegistro();
+                        if (!resultado) {
+                            let funcionario = await Funcionario.criar(req, body);
+                            return res.json(funcionario);
+                        } else {
+                            return res.json({
+                                error: 'funcionario já cadastrado'
+                            })
+                        }
+                    } catch (err) {
+                        next(err);
+
+                    }
+                }
+                exports.buscarTodos = async(req, res, next) => {
+                    try {
+                        const todos = await Funcionario.buscarTodos();
+                        return res.json(todos);
+                    } catch (err) {
+                        next(err);
+                    }
+                }
